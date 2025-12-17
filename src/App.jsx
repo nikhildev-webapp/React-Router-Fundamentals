c04f0891f923f444127a115cb031ab9133e6eb11
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-17 17:14:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 style={{textAlign:'center'}}>React Router Dom Exercises & Mini Project</h1>
+    </>
+  )
+}
+
+export default App
