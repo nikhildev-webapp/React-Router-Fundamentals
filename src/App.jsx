1e8660a4dd4ecb45704824d7e78112dc81835abb
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-18 17:34:12 +0530

    Comment the old code and create the components folder to practice and do exercise of react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,50 @@
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import './App.css'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Dashboard from './Pages/Dashboard'
-import Settings from './Pages/Settings'
-import Navbar from './Pages/Navbar'
-import NotFound from './Pages/NotFound'
-import Product from './Pages/Product'
-import ProductDetails from './Pages/ProductDetails'
+// import Home from './Pages/Home'
+// import About from './Pages/About'
+// import Dashboard from './Pages/Dashboard'
+// import Settings from './Pages/Settings'
+// import Navbar from './Pages/Navbar'
+// import NotFound from './Pages/NotFound'
+// import Product from './Pages/Product'
+// import ProductDetails from './Pages/ProductDetails'
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Navbar />,
-      children: [
-        { index: true, element: <Home /> },
+//   const router = createBrowserRouter([
+//     {
+//       path: '/',
+//       element: <Navbar />,
+//       children: [
+//         { index: true, element: <Home /> },
        
-         {
-      path: '/about',
-      element: <About />
-    },
-    {
-      path: '/dashboard',
-      element:<Dashboard/>
-    },
-    {
-      path: '/settings',
-      element:<Settings/>
-        },
-        {
-          path:'/product',
-          element:<Product/>
-        },
-        {
-      path:'/product/:id',
-      element:<ProductDetails/>
-    },
-    {
-      path: '*',
-      element:<NotFound/>
-    }
-      ]
-    },
+//          {
+//       path: '/about',
+//       element: <About />
+//     },
+//     {
+//       path: '/dashboard',
+//       element:<Dashboard/>
+//     },
+//     {
+//       path: '/settings',
+//       element:<Settings/>
+//         },
+//         {
+//           path:'/product',
+//           element:<Product/>
+//         },
+//         {
+//       path:'/product/:id',
+//       element:<ProductDetails/>
+//     },
+//     {
+//       path: '*',
+//       element:<NotFound/>
+//     }
+//       ]
+//     },
    
- ])
+//  ])
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>React Router Dom Exercises & Mini Project</h1>
