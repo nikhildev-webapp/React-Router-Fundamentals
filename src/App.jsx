5750fa876ade77339191e846bcb70632c69b0c39
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-18 16:12:03 +0530

    react-router example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import About from './Pages/About'
 function App() {
-
+  const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    {path:'/about',element:<About/>}
+ ])
   return (
     <>
-      <h1 style={{textAlign:'center'}}>React Router Dom Exercises & Mini Project</h1>
+      <h1 style={{ textAlign: 'center' }}>React Router Dom Exercises & Mini Project</h1>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
