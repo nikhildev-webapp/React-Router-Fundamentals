cbfaf9152271a8b220fc82aa694c070b1379f85d
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-19 16:34:58 +0530

    App.jsx
    remove comments from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ return (
     <>
       <h1 style={{ textAlign: 'center' }}>React Router Dom Exercises & Mini Project</h1>
     <RouterProvider router={router}></RouterProvider>
-    {/* start the context api create folder and add in github */}
+    
     </>
   )
 }
